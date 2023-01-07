41e01e6420692f83987e9ea87c662626a09b115f
eziraa <ezratgab@gmail.com>
2023-01-07 05:50:08 +0300

    commited

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*
+ * File: holberton.h
+ * Auth: Firdaus
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libholberton.a.
+ */
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +28,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
