bb6fcce41b057461ae2cd1d8822579b96e5ff851
eziraa <ezratgab@gmail.com>
2023-02-03 01:45:48 +0300

    commited

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, m = 1;/*string length*/
+	unsigned int result = 0;
+
+	/*if b is null return 0*/
+	if (!b)
+		return (0);
+
+	/*loop thru the string*/
+	while (*(b + i) != '\0')
+	{
+		/*it the current character is not 1 or 0 return 0*/
+		if (*(b + i) != '0' && *(b + i) != '1')
+			return (0);
+
+		i++;
+	}
+
+	--i;
+
+	while (i >= 0)
+	{
+		result += (*(b + i) - '0') * m;
+		m *= 2;
+		--i;
+	}
+
+	return (result);
+}
---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdint.h>
+
+/**
+ * get_endianness - checks the endianness.
+ * Return: void
+ */
+
+int get_endianness(void)
+{
+	unsigned int x = 0x76543210;
+	char *c = (char *)&x;
+
+	return (*c == 0x10);
+}
